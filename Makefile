04b99924316c6f50bf9d7a10533b26c2d00924da
jPee2k <kipishept@gmail.com>
2020-06-10 13:03:30 +0300

    Added Practice Router

--- Makefile
+++ Makefile
@@ -1,5 +1,8 @@
 start:
 	php -S localhost:8080 -t public public/index.php
 
+practice:
+	php -S localhost:8080 -t public public/practice.php
+
 restart:
 	sudo service apache2 restart
--- public/index.php
+++ public/index.php
@@ -4,9 +4,19 @@
 require __DIR__ . '/../vendor/autoload.php';
 
 use Slim\Factory\AppFactory;
+use DI\Container;
 
-$app = AppFactory::create();
+$container = new Container();
+$container->set('renderer', function () {
+    // Параметром передается базовая директория в которой будут храниться шаблоны
+    return new \Slim\Views\PhpRenderer(__DIR__ . '/../templates');
+});
+
+//$app = AppFactory::create();
+$app = AppFactory::createFromContainer($container);
 $app->addErrorMiddleware(true, true, true);
+
+// ------ INDEX ------ //
 
 $app->get('/', function ($request, $response) {
     return $response->write('GET /');
@@ -14,9 +24,24 @@ $app->get('/', function ($request, $response) {
     // return $response->write('Welcome to Slim!');
 });
 
+// ------ USERS ------ //
+
 $app->get('/users', function ($request, $response) {
     return $response->write('GET /users');
 });
+
+$app->get('/users/{id}', function ($request, $response, $args) {
+    $params = ['id' => $args['id'], 'nickname' => 'user-' . $args['id']];
+    // Указанный путь считается относительно базовой директории для шаблонов, заданной на этапе конфигурации
+    // $this доступен внутри анонимной функции благодаря https://php.net/manual/ru/closure.bindto.php
+    return $this->get('renderer')->render($response, 'users/show.phtml', $params);
+});
+
+$app->post('/users', function ($request, $response) {
+    return $response->withStatus(302);
+});
+
+// ------ COMPANIES ------ //
 
 $app->get('/companies', function ($request, $response) {
     return $response->write('GET /companies');
@@ -26,9 +51,7 @@ $app->post('/companies', function ($request, $response) {
     return $response->write('POST /companies');
 });
 
-$app->post('/users', function ($request, $response) {
-    return $response->withStatus(302);
-});
+// ------ COURSES ------ //
 
 $app->get('/courses/{id}', function ($request, $response, array $args) {
     $id = $args['id'];
